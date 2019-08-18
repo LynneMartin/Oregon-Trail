317bf74b2827980c586fd9ccbe19d1a3f137b975
LynneMartin <lynnemartin@gmail.com>
2019-08-18 16:59:52 -0600

    constructor added in item.cs

--- Project/Models/Item.cs
+++ Project/Models/Item.cs
@@ -7,5 +7,11 @@ namespace OregonTrail.Project.Models
   {
     public string Name { get; set; }
     public string Description { get; set; }
+
+    public Item(string name, string description)
+    {
+      Name = name;
+      Description = description;
+    }
   }
 }
